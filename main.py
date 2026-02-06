f5d79652efdaf609cfd141140785fc12a27d0eb5
SamW-hash <samvincentwilliams@gmail.com>
2026-02-05 20:38:41 -0800

    Prompts are now stored in a list that can be used by the llm for context in longer conversations

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
+
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("api_key not found: check your .env")
@@ -11,7 +13,8 @@ client = genai.Client(api_key=api_key)
 parser = argparse.ArgumentParser(description="Agent")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
-response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
 if response.usage_metadata.candidates_token_count == None:
     raise RuntimeError("usage_metadata not available: likely failed API request")
