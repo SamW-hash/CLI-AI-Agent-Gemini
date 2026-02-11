049033588db26a4ad0b696e9e3c4a16f9dd98543
SamW-hash <samvincentwilliams@gmail.com>
2026-02-10 23:00:06 -0800

    Added run_python_file and appropriate tests

---
+++ functions/run_python_file.py
@@ -0,0 +1,38 @@
+import os, subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        if not valid_target_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not target_dir.endswith('.py'):
+            return f'Error: "{file_path}" is not a Python file'
+        
+        command = ["python", target_dir]
+        if args != None:
+            command.extend(args)
+        result = subprocess.run(
+            command, 
+            cwd=working_dir_abs, 
+            capture_output=True,
+            text=True,
+            timeout=30)
+        output_message = []
+        if result.returncode != 0:
+            output_message.append(f"Process exited with code {result.returncode}")
+        if not result.stderr and not result.stdout:
+            output_message.append("No output produced")
+        if result.stdout:
+            output_message.append(f"STDOUT: {result.stdout}")
+        if result.stderr:
+            output_message.append(f"STDERR: {result.stderr}")
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    return "".join(output_message)
+
+        
